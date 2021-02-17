58451fa4ce4c9a6a53dc75a1da821ad31b074a8a
tomek1911 <tmk.szczepanski@gmail.com>
2021-02-17 17:47:54 +0100

    setup project

---
+++ data/download_mnist.sh
@@ -0,0 +1,4 @@
+curl -O -J http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+curl -O -J http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+curl -O -J http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+curl -O -J http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
