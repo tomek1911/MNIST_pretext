fd6ab0fe8a22e1b2c018fc7717a2c69c7f78f8a6
tomek1911 <tmk.szczepanski@gmail.com>
2021-02-17 23:36:08 +0100

    test basic model for mnist

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,6 +2,7 @@ from torchvision.datasets import MNIST
 from torch.utils.data.dataloader import DataLoader
 import torchvision.transforms 
 
+
 class MNISTDataLoader:
     """MNIST dataset"""
 
@@ -11,18 +12,18 @@ class MNISTDataLoader:
     test_dataset = None 
     mytransform = None
 
-    def __init__(self):
+    def __init__(self, path, isDownload, training_batch):
 
         self.mytransform = torchvision.transforms.Compose(
             [torchvision.transforms.ToTensor(),
              torchvision.transforms.Normalize(
                  mean = (0.1307,), std=(0.3081,))]
         )
-        self.train_dataset = MNIST('../data/', train=True, download = True, transform=self.mytransform)
-        self.test_dataset = MNIST('../data/', train=False, download = True, transform=self.mytransform)
+        self.train_dataset = MNIST(path, train=True, download = isDownload, transform=self.mytransform)
+        self.test_dataset = MNIST(path, train=False, download = isDownload, transform=self.mytransform)
 
-        self.train_loader = DataLoader(self.train_dataset, batch_size=64, shuffle=True)
-        self.test_loader = DataLoader(self.test_dataset, batch_size=64, shuffle=False)
+        self.train_loader = DataLoader(self.train_dataset, batch_size=training_batch, shuffle=True)
+        self.test_loader = DataLoader(self.test_dataset, batch_size=1024, shuffle=False)
         
 
         
--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
+import torch
+import torch.optim as optim
+
 import dataloader
-import pretext_models.model
 import matplotlib.pyplot as plt
 
-def main():
+random_seed = 1
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
 
-    loader = dataloader.MNISTDataLoader()
-    examples = enumerate(loader.test_loader)
-    batch_idx, (example_data, example_targets) = next(examples)
-    fig = plt.figure()    
-    for i in range(6):
-        plt.subplot(2,3,i+1)
-        plt.tight_layout()
-        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-        plt.title("Ground Truth: {}".format(example_targets[i]))
-        plt.xticks([])
-        plt.yticks([])
-    plt.show()
+network = None
+optimizer = None
+loader = None
+
+def main():
+    # examples = enumerate(loader.test_loader)
+    # batch_idx, (example_data, example_targets) = next(examples)
+    # fig = plt.figure()    
+    # for i in range(6):
+    #     plt.subplot(2,3,i+1)
+    #     plt.tight_layout()
+    #     plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+    #     plt.title("Ground Truth: {}".format(example_targets[i]))
+    #     plt.xticks([])
+    #     plt.yticks([])
+    # plt.show()
+    pass
 
 if __name__ == "__main__":
     main()
