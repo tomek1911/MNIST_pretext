7d0b4ac47f903c23f06fc211e112b87b276f3214
tomek1911 <tmk.szczepanski@gmail.com>
2021-02-18 17:38:03 +0100

    add validation

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,3 +1,4 @@
+import torch
 from torchvision.datasets import MNIST
 from torch.utils.data.dataloader import DataLoader
 import torchvision.transforms 
@@ -8,7 +9,10 @@ class MNISTDataLoader:
 
     train_loader = None
     test_loader = None
+    validation_loader= None
+    
     train_dataset = None
+    validaton_dataset = None
     test_dataset = None 
     mytransform = None
 
@@ -17,13 +21,17 @@ class MNISTDataLoader:
         self.mytransform = torchvision.transforms.Compose(
             [torchvision.transforms.ToTensor(),
              torchvision.transforms.Normalize(
-                 mean = (0.1307,), std=(0.3081,))]
-        )
-        self.train_dataset = MNIST(path, train=True, download = isDownload, transform=self.mytransform)
+                 mean = (0.5,), std=(1,))]
+        ) 
+        train = MNIST(path, train=True, download = isDownload, transform=self.mytransform)
+        self.train_dataset, self.validaton_dataset = torch.utils.data.random_split(
+        train, [50000, 10000], generator=torch.Generator().manual_seed(1))
+        
         self.test_dataset = MNIST(path, train=False, download = isDownload, transform=self.mytransform)
-
+       
         self.train_loader = DataLoader(self.train_dataset, batch_size=training_batch, shuffle=True)
-        self.test_loader = DataLoader(self.test_dataset, batch_size=1024, shuffle=False)
+        self.validation_loader = DataLoader(self.validaton_dataset, batch_size=1000, shuffle=True)
+        self.test_loader = DataLoader(self.test_dataset, batch_size=1000, shuffle=False)
         
 
         
--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 import time
 import torch.optim
 import torch.nn.functional as F
+from torchsummary import summary
 
 from dataloader import MNISTDataLoader
 from pretext_models.pilot_model import Net
@@ -20,11 +21,13 @@ my_device = torch.device("cuda:0" if use_cuda else "cpu")
 print('Using: ', my_device)
 
 network = Net().to(device=my_device)
-optimizer = torch.optim.SGD(network.parameters(), lr = 0.01, momentum=0.5, weight_decay=1e-3)
+summary(network, (1, 28, 28))
+ 
+optimizer = torch.optim.SGD(network.parameters(), lr=0.01, momentum=0.9)
 loader = MNISTDataLoader('../data', True, training_batch)
 train_loader = loader.train_loader
 test_loader = loader.test_loader
-
+validation_loader = loader.validation_loader
 train_losses = []
 train_counter = []
 test_losses = []
@@ -37,7 +40,7 @@ def train(epoch):
     optimizer.zero_grad()
     data, target = data.to(my_device), target.to(my_device)
     output = network(data)
-    loss = F.nll_loss(output, target)
+    loss = F.cross_entropy(output, target)
     loss.backward()
     optimizer.step()
 
@@ -52,6 +55,22 @@ def train(epoch):
         torch.save(network.state_dict(), './results/model.pth')
         torch.save(optimizer.state_dict(), './results/optimizer.pth')
 
+def validation():
+  valid_loss = 0
+  correct = 0
+  with torch.no_grad():
+    for data, target in validation_loader:
+      data, target = data.to(my_device), target.to(my_device)  
+      output = network(data)
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += pred.eq(target.data.view_as(pred)).sum()
+      valid_loss += F.cross_entropy(output, target, size_average=False).item()
+    valid_loss /= len(validation_loader.dataset)
+    print('\nValidation set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    valid_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
+
+
 def test():
   network.eval()
   test_loss = 0
@@ -60,7 +79,7 @@ def test():
     for data, target in test_loader:
       data, target = data.to(my_device), target.to(my_device)  
       output = network(data)
-      test_loss += F.nll_loss(output, target, size_average=False).item()
+      test_loss += F.cross_entropy(output, target, size_average=False).item()
       pred = output.data.max(1, keepdim=True)[1]
       correct += pred.eq(target.data.view_as(pred)).sum()
   test_loss /= len(test_loader.dataset)
@@ -69,11 +88,15 @@ def test():
     test_loss, correct, len(test_loader.dataset),
     100. * correct / len(test_loader.dataset)))
 
+
+
 test()
 start_time = time.time()
 for epoch in range(1, n_epochs + 1):
   train(epoch)
-  test()
+  validation()
+
+test()
 
 print('Train time: {:.2f}s'.format(time.time() - start_time))
 
