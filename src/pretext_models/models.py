1a1d002f83b37d82766497dde0569fab642799b4
tomek1911 <tmk.szczepanski@gmail.com>
2021-02-24 18:57:10 +0100

    add weights init

--- src/pretext_models/models.py
+++ src/pretext_models/models.py
@@ -12,6 +12,7 @@ class Net(nn.Module):
         self.pool = nn.MaxPool2d(2,2)
         self.fc1 = nn.Linear(20*6*6, 50)
         self.fc2 = nn.Linear(50, 10)
+        self._initialize_weights()
 
     def forward(self, x):
         x = F.relu(self.pool(self.conv1(x)))
@@ -21,6 +22,19 @@ class Net(nn.Module):
         x = F.dropout(x, training=self.training, p=0.5)
         x = self.fc2(x)
         return x
+    
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
 class NetVgg(nn.Module):
     def __init__(self, features, num_classes = 10, init_weights=True):
